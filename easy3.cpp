feac61780d313fbe4a9bef5f5674d22453176530
7London7 <106383366+7London7@users.noreply.github.com>
2023-09-19 13:19:03 +0300

    Update easy3.cpp

--- easy3.cpp
+++ easy3.cpp
@@ -9,7 +9,7 @@ double itc_fmin(double num, double num2){
 }
 int itc_sqrt(int num){
     for (long long i = 0; i < num; i++){
-        if(num - i * i == 0){
+        if(i * i == 0){
             return i;
         }
     }
